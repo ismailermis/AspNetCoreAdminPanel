cb6b2722f476f5a0304a6433364e93de80c05c04
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2021-03-30 23:32:59 +0300

    StaticLengths and StringExtention was added

---
+++ src/Libraries/Core/Helpers/StaticLengths.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Helpers
+{
+    public static class StaticLengths
+    {
+        public const int Address1 = 50;
+
+        public const int Address2 = 15;
+
+        public const int City = 30;
+
+        public const int Description = 100;
+
+        public const int FirstName = 25;
+
+        public const int LastName = 35;
+
+        public const int MiddleName = 25;
+
+        public const int Name = 50;
+
+        public const int SocialSecurityNumber = 11;
+
+    }
+}
---
+++ src/Libraries/Core/Helpers/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Core.Helpers
+{
+    public static class StringExtensions
+    {
+        private static readonly Regex digitsRegex = new Regex(@"\d", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        private static readonly Regex nonDigitsRegex = new Regex(@"\D", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        public static string ReplaceDigits(this string text, string replacement) => digitsRegex.Replace(text ?? "", replacement);
+
+        public static string StripNonDigits(this string text) => nonDigitsRegex.Replace(text ?? "", "");
+    }
+}
