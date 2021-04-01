3fdd54b194e01a049beb1c4af8b598ea5dcd5cf7
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2021-04-01 11:40:11 +0300

    ProductSpecifications was added

---
+++ src/Libraries/Data/Specifications/ProductSpecification/ProductWithFiltersForCountSpecificication.cs
@@ -0,0 +1,16 @@
+﻿using Models.DbEntities;
+
+namespace Data.Specifications.ProductSpecification
+{
+    public class ProductWithFiltersForCountSpecificication : BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecificication(ProductSpecParams productParams)
+            : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
+        {
+        }
+    }
+}
